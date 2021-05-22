d4b28960ed0e530df5bf1ccd7e1cc93a44c51275
Petro Moldovan <petro.moldovan@dcso.de>
2021-05-22 13:22:52 +0200

    f

--- src/Main.java
+++ src/Main.java
@@ -32,99 +32,6 @@ public class Main extends Application {
         launch(args);
     }
 
-    private AnimationTimer timer;
-
-    private Pane root;
-    private List<Node> cars = new ArrayList<>();
-    private Node frog;
-    //private int frogSize = 39;
-    private int startPosition = HEIGHT - 39;
-
-
-    private Parent createContent() {
-        root = new Pane();
-        root.setPrefSize(WIDTH, HEIGHT);
-
-        frog = initFrog();
-        root.getChildren().add(frog);
-
-        timer = new AnimationTimer() {
-            @Override
-            public void handle(long now) {
-                onUpdate();
-            }
-        };
-        timer.start();
-
-        return root;
-    }
-
-    private void onUpdate() {
-        // update positon of all cars
-        for (Node car : cars)
-            car.setTranslateX(car.getTranslateX() + Math.random() * 10);
-
-        // add a car
-        if (Math.random() < 0.08) {
-            cars.add(initCar());
-        }
-
-        // check for collision
-        checkState();
-    }
-
-    private void checkState() {
-        for (Node car : cars) {
-            if (car.getBoundsInParent().intersects(frog.getBoundsInParent())) {
-                // game over. reset frog
-                //frog.setTranslateX(0);
-                frog.setTranslateX((int)(WIDTH/2));
-                frog.setTranslateY(startPosition);
-            }
-        }
-
-        // check if the other end is reached
-        if (frog.getTranslateY() <= 10) {
-            timer.stop();
-
-            String win = "YOU WIN";
-            HBox hBox = new HBox();
-            hBox.setTranslateX(300);
-            hBox.setTranslateY(250);
-            root.getChildren().add(hBox);
-            for (int i = 0; i < win.toCharArray().length; i++) {
-                char letter = win.charAt(i);
-                Text text = new Text(String.valueOf(letter));
-                text.setFont(Font.font(50));
-                text.setOpacity(0);
-
-                hBox.getChildren().add(text);
-                FadeTransition ft = new FadeTransition(Duration.seconds(0.66), text);
-                ft.setToValue(1);
-                ft.setDelay(Duration.seconds(i * 0.15));
-                ft.play();
-            }
-        }
-    }
-
-    private Node initFrog() {
-        Image image = new Image("./resources/assets/textures/frog.png");
-        Rectangle rect = new Rectangle(38,38, Color.TRANSPARENT);
-        rect.setTranslateY(startPosition);
-        rect.setTranslateX((int)(WIDTH/2));
-        ImagePattern imagePattern = new ImagePattern(image);
-        rect.setFill(imagePattern);
-        return rect;
-    }
-
-    private Node initCar() {
-        Rectangle rect = new Rectangle(40,40, Color.RED);
-                                                //14 rows
-        rect.setTranslateY((int)(Math.random() * 14) * 40);
-        root.getChildren().add(rect);
-        return rect;
-    }
-
     @Override
     public void start(Stage primaryStage) throws Exception{
         window = primaryStage;
@@ -139,38 +46,8 @@ public class Main extends Application {
         screenController.add("board", FXMLLoader.load(getClass().getResource( "./resources/board.fxml" )));
         screenController.activate("home");
 
-
-        //Scene gameScreen = new Scene(createContent(), WIDTH, HEIGHT);
-
         // render registration scene
         window.setScene(rootScreen);
-
-//        window.getScene().setOnKeyPressed(event -> {
-//            double newPosition;
-//            switch (event.getCode()) {
-//                case W:
-//                    frog.setTranslateY(frog.getTranslateY() - 40);
-//                    break;
-//                case S:
-//                    newPosition = frog.getTranslateY() + 40;
-//                    if (newPosition > HEIGHT) return;
-//                    frog.setTranslateY(newPosition);
-//                    break;
-//                case A:
-//                    newPosition = frog.getTranslateX() - 40;
-//                    if (newPosition < 0) return;
-//                    frog.setTranslateX(newPosition);
-//
-//                    break;
-//                case D:
-//                    newPosition = frog.getTranslateX() + 40;
-//                    if (newPosition >= WIDTH) return;
-//                    frog.setTranslateX(newPosition);
-//                    break;
-//                default:
-//                    break;
-//            }
-//        });
 
         window.show();
     }
